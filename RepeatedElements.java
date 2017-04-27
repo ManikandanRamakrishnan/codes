f8dec37d592ad6ea07ab769f6f3eae850627ca6c
ManikandanRamakrishnan <manikandanrk97@gmail.com>
2017-04-26 21:55:53 -0700

    Add files via upload

---
+++ RepeatedElements.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class RepeatedElements {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		Set<Integer> set=new HashSet<>();
+		ArrayList<Integer> number= new ArrayList<Integer>();
+		System.out.println("Enter registration numbers");
+		int n=input.nextInt();
+		System.out.println("Enter registration numbers");
+		for(int i=0;i<n;i++)
+		{
+			int k=input.nextInt();
+			number.add(k);
+		}
+		System.out.println("The duplicate registration numbers are");
+		for(int i=0;i<n;i++)
+		{
+			if(!set.add(number.get(i))){
+				System.out.print(number.get(i)+" ");	
+			}
+		}
+		input.close();
+
+	}
+
+}
