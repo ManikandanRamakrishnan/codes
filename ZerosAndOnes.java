7484dab724ed7d53302161e17ddeaf2f00a486e3
ManikandanRamakrishnan <manikandanrk97@gmail.com>
2017-05-02 09:51:06 -0700

    Add files via upload

---
+++ ZerosAndOnes.java
@@ -0,0 +1,50 @@
+package Array;
+
+import java.util.Scanner;
+
+public class ZerosAndOnes {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter Array1 Size");
+        int n=input.nextInt();
+        int[][] arr=new int[n][n];
+        int[][] copy=new int[n][n];
+        for(int i=0;i<n;i++){
+        	for(int j=0;j<n;j++)
+        	{
+        		arr[i][j]=input.nextInt();
+        		copy[i][j]=arr[i][j];
+        	}
+        }
+        for(int i=0;i<n;i++){
+        	for(int j=0;j<n;j++)
+        	{
+        		if(arr[i][j]==0)
+        		{
+        			for(int z=0;z<n;z++)
+        			{
+        				copy[z][j]=0;
+        			}
+        		}
+        		if(arr[i][j]==0)
+        		{
+        			for(int z=0;z<n;z++)
+        			{
+        				copy[i][z]=0;
+        			}
+        		}
+        	}
+        }
+        System.out.println("The output array is");
+        for(int i=0;i<n;i++){
+        	for(int j=0;j<n;j++)
+        	{
+            System.out.print(copy[i][j]+" ");
+        	}
+        	System.out.println();
+        }
+        input.close();
+	}
+
+}
